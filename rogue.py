88d2f616a99d44a0af853fd1d06d6bc61ed75429
Marco Tompitak <16779581+marcotompitak@users.noreply.github.com>
2018-02-24 14:33:05 +0100

    Added basic signing and verification of requests to the mutual_add endpoint, to prevent spoofing.
    Added a JSON schema to verify the format of the more complicated mutual_add requests.
    
    Added a starter script for a rogue client. This first version just tries to spoof a mutual_add request, and fails because of the measure described above.

---
+++ rogue.py
@@ -0,0 +1,26 @@
+from pki import get_kp, pubkey_to_addr, sign, verify
+import requests
+import json
+
+
+def standard_encode(dictionary):
+    return bytes(json.dumps(dictionary, sort_keys=True, separators=(',', ':')), 'utf-8')
+
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+# Make a mutual_add request pretending to be another host
+
+target = config['seeds'][0]
+spoof = config['seeds'][1]
+spoof_port = spoof.split(':')[2]
+
+pub, priv = get_kp()
+
+content = {"port": int(spoof_port), 'pubkey': pub}
+signature = sign(standard_encode(content), priv)
+content['signature'] = signature
+
+response = requests.post(target + '/mutual_add', json=content, timeout=1000)
+print(response.text)
